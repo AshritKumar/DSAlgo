1b9696db11c68c031ebb18e6d57ecea70b962084
Ashrit Kumar Samudrala <samudrala.ashrit@gmail.com>
2019-01-15 20:01:20 +0530

    min edit distance

--- practice/src/practice/algo/MinimumEditDistance.java
+++ practice/src/practice/algo/MinimumEditDistance.java
@@ -1,10 +1,30 @@
 package practice.algo;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.IntStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MinimumEditDistance {
+	
+	//static final String[] dictWords  = {"ashrit", "ransom", "gas", "mass", "national", "nation", "java", "python", "international", "calender",
+										//"mini", "venom", "stubborn", "vehicle", "google", "emotion", "to", "too", "ash", "as", "frustration", "spring", "kumar"};
+	static List<String> dictWords;
+	
+	public static List<String> getFileWords() {
+		if(dictWords == null) {
+			try {
+				dictWords = Files.lines(Paths.get(System.getProperty("user.dir")+"\\src\\wordlist2.txt")).collect(Collectors.toList());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return dictWords;
+	}
 	
 	public static int minimumEditDistanceRecursive(String s1, String s2, int s1len, int s2len) {
 		if(s1len == 0)
@@ -50,7 +70,7 @@ public class MinimumEditDistance {
 		}
 		
 		//printArray(distArr);
-		printEditSteps(distArr, s1, s2);
+		//printEditSteps(distArr, s1, s2);
 		return distArr[s1.length()][s2.length()];
 	}
 	
@@ -103,9 +123,45 @@ public class MinimumEditDistance {
 		return minimumEditDistanceRecursive(s1, s2, s1.length(), s2.length());
 	}
 	
+	public static List<String> getNearestWords_1(String w, int minDist) {
+		//HashMap<Integer, List<String>> nearstMap = new HashMap<>();
+		StringBuffer sb = new StringBuffer();
+		for(String s : getFileWords()) {
+			int dist = minumEditDistanceDynamic(w, s);
+			if(dist <= minDist)
+				sb.append(s+",");
+			/*
+			 * List<String> wrdsWithDist = nearstMap.get(dist); if(wrdsWithDist == null)
+			 * wrdsWithDist = new ArrayList<String>(); wrdsWithDist.add(s);
+			 * nearstMap.put(dist, wrdsWithDist);
+			 */
+		}
+		String[] arr = sb.toString().split(",");
+		return Arrays.stream(arr, 0, arr.length)
+				.collect(Collectors.toList());
+	}
+	
+	public static List<String> getNearestWords(String w, int minDist) {
+		return getFileWords().parallelStream()
+						.filter(s -> minumEditDistanceDynamic(s, w) <= minDist)
+						.collect(Collectors.toList());
+	}
+	
 	
 	public static void main(String[] args) {
-		System.out.println(minumEditDistanceDynamic("GTATGXYKJAHYTSYSYJSYSKJSYSUJSIUS","GTATGXYKHYTSYSKJSYSKJSYSUJSIKS"));
+		//System.out.println(minumEditDistanceDynamic("GTATGXYKJAHYTSYSYJSYSKJSYSUJSIUS","GTATGXYKHYTSYSKJSYSKJSYSUJSIKS"));
+		
+		  long s = System.currentTimeMillis();
+		  System.out.println(getNearestWords("kran", 1)); long e =
+		  System.currentTimeMillis(); System.out.println(((double) e-s));
+		  System.out.println("-----------"); s = System.currentTimeMillis();
+		  System.out.println(getNearestWords("ant", 1)); e =
+		  System.currentTimeMillis(); System.out.println(((double) e-s));
+		  
+		  System.out.println("-----------"); s = System.currentTimeMillis();
+		  System.out.println(getNearestWords("duc", 1)); e =
+		  System.currentTimeMillis(); System.out.println(((double) e-s));
+		 
         
 	}
 
